0c8407810f41964ea30fa89de2ddf1c46d7a18ba
Daniil <90925204+DANiCH-E@users.noreply.github.com>
2024-07-10 12:26:49 +0300

    add joystick and builds

---
+++ Assets/Scripts/Movement/JoystickPlayerMovementDirectionController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Shooter.Movement
+{
+    public class JoystickPlayerMovementDirectionController : MonoBehaviour, IMovementDirectionSource
+    {
+        private UnityEngine.Camera _camera;
+        private Joystick _joystick;
+
+        public Vector3 MovementDirection { get; private set; }
+
+        protected void Awake()
+        {
+            _joystick = FindObjectOfType<Joystick>();
+            _camera = UnityEngine.Camera.main;
+        }
+
+        void Update()
+        {
+            var horizontal = _joystick.Horizontal;
+            var vertical = _joystick.Vertical;
+
+            var direction = new Vector3(horizontal, 0, vertical);
+
+            direction = _camera.transform.rotation * direction;
+            direction.y = 0;
+
+            MovementDirection = direction.normalized;
+
+            
+        }
+    }
+}
